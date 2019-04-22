83a78155726d4a247392ad0dd3d1e9d43ad0370c
benn0348 <46505638+benn0348@users.noreply.github.com>
2019-04-21 20:53:43 -0400

    final updates again

--- lib2024/final/scripts/script.js
+++ lib2024/final/scripts/script.js
@@ -12,13 +12,9 @@ function showInput() {
         document.getElementById('user-email').innerHTML = 
                     document.getElementById("email").value;       
         document.getElementById('user-add').innerHTML = 
-                    document.getElementById("Address").value;
-        document.getElementById('user-city').innerHTML = 
-                     document.getElementById("city").value;
+                    (document.getElementById("Address").value)+", "+(document.getElementById("city").value);
         document.getElementById('user-prov').innerHTML = 
-                    document.getElementById("Province").value;    
-        document.getElementById('user-postal').innerHTML = 
-                    document.getElementById("postal-code").value; 
+                    (document.getElementById("Province").value)+", "+(document.getElementById("postal-code").value);    
         document.getElementById('mailto').href = 
                     "mailto:"+(document.getElementById("email").value);
                 }           
