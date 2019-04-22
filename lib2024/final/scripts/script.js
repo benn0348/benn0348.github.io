0d04f60ff98c6114326a98c458be46038dc59681
benn0348 <46505638+benn0348@users.noreply.github.com>
2019-04-21 20:43:46 -0400

    final udates-still in progress

--- lib2024/final/scripts/script.js
+++ lib2024/final/scripts/script.js
@@ -1,2 +1,25 @@
 window.onload = init;
 
+function RachaelFunction() {
+	alert("HERRRROOOO?????");
+	alert("HEEEERROOOOOOOO????");
+	alert("HARRROOOOOOOOOOOOOOO??????");
+	alert("merrrrrp");
+}
+function showInput() {
+        document.getElementById('user-names').innerHTML = 
+                    document.getElementById("Name").value;
+        document.getElementById('user-email').innerHTML = 
+                    document.getElementById("email").value;       
+        document.getElementById('user-add').innerHTML = 
+                    document.getElementById("Address").value;
+        document.getElementById('user-city').innerHTML = 
+                     document.getElementById("city").value;
+        document.getElementById('user-prov').innerHTML = 
+                    document.getElementById("Province").value;    
+        document.getElementById('user-postal').innerHTML = 
+                    document.getElementById("postal-code").value; 
+        document.getElementById('mailto').href = 
+                    "mailto:"+(document.getElementById("email").value);
+                }           
+
