2d41228550136dad82f8a84b8027bbe3bd836fd2
benn0348 <46505638+benn0348@users.noreply.github.com>
2019-04-22 20:12:40 -0400

    final submission

--- lib2024/final/scripts/script.js
+++ lib2024/final/scripts/script.js
@@ -1,12 +1,21 @@
-window.onload = init;
-
-function RachaelFunction() {
-	alert("HERRRROOOO?????");
-	alert("HEEEERROOOOOOOO????");
-	alert("HARRROOOOOOOOOOOOOOO??????");
-	alert("merrrrrp");
+var loading; 
+function loadinganimation() {
+        loading = setTimeout(showInput,3000);
+        document.getElementById("user-names").style.display = "none";
+        document.getElementById("user-email").style.display = "none";
+        document.getElementById("user-add").style.display = "none";
+        document.getElementById("user-prov").style.display = "none";
+        document.getElementById("mailto").style.display = "none";
+        document.getElementById("user-circle").style.display = "none";
+        document.getElementById("loader").style.display = "block";
 }
 function showInput() {
+        document.getElementById("user-circle").style.display = "block";
+        document.getElementById("user-names").style.display = "block";
+        document.getElementById("user-email").style.display = "block";
+        document.getElementById("user-add").style.display = "block";
+        document.getElementById("user-prov").style.display = "block";
+        document.getElementById("mailto").style.display = "block";
         document.getElementById('user-names').innerHTML = 
                     document.getElementById("Name").value;
         document.getElementById('user-email').innerHTML = 
@@ -17,5 +26,5 @@ function showInput() {
                     (document.getElementById("Province").value)+", "+(document.getElementById("postal-code").value);    
         document.getElementById('mailto').href = 
                     "mailto:"+(document.getElementById("email").value);
+        document.getElementById("loader").style.display = "none";
                 }           
-
